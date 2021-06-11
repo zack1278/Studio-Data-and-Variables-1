e7fb15b1df14f66a54d73fdc5d77d4152cc029be
zach127 <85476516+zack1278@users.noreply.github.com>
2021-06-11 00:54:46 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,49 @@
+let date = "Monday 2019-03-18";
+
+let time = "10:05:34 AM";
+
+let astronautCount = 7;
+
+const astronautStatus = "ready";
+
+let averageAstronautMassKg = 80.7;
+
+const crewMassKg = astronautCount * averageAstronautMassKg;
+
+let fuelMassKg ="760,000";
+
+let shuttleMassKg	= 74842.31;
+
+const totalMassKg	= crewMassKg + fuelMassKg + shuttleMassKg;
+
+let fuelTempCelsius = (-225);
+
+let fuelLevel = ("100%");
+
+let weatherStatus = "clear";
+
+
+console.log(date);
+console.log("=================");
+console.log(time);
+console.log("=================");
+console.log(astronautCount);
+console.log("=================");
+console.log(astronautStatus);
+console.log("=================");
+console.log(averageAstronautMassKg);
+console.log("=================");
+console.log(crewMassKg);
+console.log("=================");
+console.log(fuelMassKg);
+console.log("=================");
+console.log(shuttleMassKg);
+console.log("=================");
+console.log(totalMassKg);
+console.log("=================");
+console.log(fuelTempCelsius);
+console.log("=================");
+console.log(fuelLevel);
+console.log("=================");
+console.log(weatherStatus);
+console.log("=================");
